760f21ae1540874b7ddc56a9024f6b8218198d3f
Kai Dev <lenanghai@gmail.com>
2015-04-30 20:18:20 +0700

    Create kai_experpt.php

---
+++ WP_Class/kai_experpt.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+* Kai Excerpt with limit content
+* copy to functions.php in your theme
+*/
+
+
+// Truncate string to x letters/words
+function kai_truncate( $str, $length = 40, $units = 'letters', $ellipsis = '&nbsp;&hellip;' ) {
+    if ( $units == 'letters' ) {
+        if ( mb_strlen( $str ) > $length ) {
+            return mb_substr( $str, 0, $length ) . $ellipsis;
+        } else {
+            return $str;
+        }
+    } else {
+        $words = explode( ' ', $str );
+        if ( count( $words ) > $length ) {
+            return implode( " ", array_slice( $words, 0, $length ) ) . $ellipsis;
+        } else {
+            return $str;
+        }
+    }
+}
+
+// Kai Excerpt
+
+if ( ! function_exists( 'kai_excerpt' ) ) {
+// Limit content param
+    function kai_excerpt( $limit = 40 ) {
+      return kai_truncate( get_the_excerpt(), $limit, 'words' );
+    }
+}
